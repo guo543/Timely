304cb9c8d27e17006b547b78cf17103637f06dbf
guo543 <guo543@purdue.edu>
2022-06-21 22:03:19 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+
+const myLogger = function (req, res, next) {
+    console.log('LOGGED')
+    next()
+}
+
+const requestTime = function (req, res, next) {
+    req.requestTime = Date.now()
+    next()
+}
+
+app.use(myLogger)
+app.use(requestTime)
+
+app.get('/', (req, res) => {
+    let responseText = 'Hello World!<br>'
+    responseText += `<small>Requested at: ${req.requestTime}</small>`
+    res.send(responseText)
+})
+
+app.listen(3000)
